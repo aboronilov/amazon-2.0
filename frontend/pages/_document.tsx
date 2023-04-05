2aa73b9b08825577da1c3beac3c6b753fbf75e30
= <=>
2023-04-05 10:50:33 +0200

    base frontend init

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+         <link rel="icon" href="/Amazon_logo.svg.png" sizes="any" type="image/svg+xml" />
+         <title>Boronilov Amazon</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
