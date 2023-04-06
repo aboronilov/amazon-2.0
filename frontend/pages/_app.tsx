45ee11f0df18a9e1fa5c8eaa42983e1084640c53
= <=>
2023-04-06 10:47:35 +0200

    auth and check role providers

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 
 import { persistor, store } from "@/store/store"
+import { TypeComponentAuthFields } from '@/providers/auth-provider/auth-pages.type'
+import AuthProvider from '@/providers/auth-provider/AuthProvider'
 
 const queryClient = new QueryClient({
    defaultOptions: {
@@ -15,12 +17,14 @@ const queryClient = new QueryClient({
    }
 })
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps & TypeComponentAuthFields) {
   return (
       <QueryClientProvider client={queryClient}>
          <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-               <Component {...pageProps} />
+               <AuthProvider Component={{isOnlyUser: Component.isOnlyUser}}>
+                  <Component {...pageProps} />
+               </AuthProvider>
             </PersistGate>
          </Provider>
       </QueryClientProvider>
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -11,8 +11,8 @@ import {
 } from "redux-persist"
 import storage from "redux-persist/lib/storage"
 
-import { carouselSlice } from "./carousel/carousel.slice"
-import { cartSlice } from "./cart/cart.slice"
+// import { carouselSlice } from "./carousel/carousel.slice"
+// import { cartSlice } from "./cart/cart.slice"
 import { userSlice } from './user/user.slice'
 
 const persistConfig = {
@@ -22,8 +22,8 @@ const persistConfig = {
 }
 
 const rootReducer = combineReducers({
-   cart: cartSlice.reducer,
-   carousel: carouselSlice.reducer,
+   // cart: cartSlice.reducer,
+   // carousel: carouselSlice.reducer,
    user: userSlice.reducer
 })
 
